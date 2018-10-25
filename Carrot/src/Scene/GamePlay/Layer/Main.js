1430bb96b23f5227f120e949c140c9e2ac41627b
wenqiu.he <wenqiu.he@yahoo.com>
2018-10-25 19:41:32 +0800

    update

--- Carrot/src/Scene/GamePlay/Layer/Main.js
+++ Carrot/src/Scene/GamePlay/Layer/Main.js
@@ -53,5 +53,156 @@ let GPMainLayer = cc.Layer.extend({
         let finalOffsetX = 0;
         let finalOffsetY = 0;
         let groupName = "";
+        for (let i = 0; i < groups.length; i++) {
+            group = groups[i];
+            groupName = group.getGroupName();
+            if (groupName == 'road' || groupName == "start_end") {
+                finalOffsetX = offsetX + this.tileSize.width / 2;
+                finalOffsetY = offsetY + this.tileSize.height / 2;
+            } else {
+                cc.warn("GPMainLayer.loadTileMap():" + groupName + "对象组的坐标未调整");
+            }
+            group.setPositionOffset(cc.p(finalOffsetX, finalOffsetY));
+        }
+    },
+    // 加载路径坐标
+    loadRoadPointArray: function () {
+        this.roadPointArray = [];
+        let roadGroup = this.tiledMap.getObjectGroup('road');
+        let roads = roadGroup.getObjects();
+        for (let i = 0; i < roads.length; i++) {
+            this.roadPointArray.push(cc.p(roads[i].x + roadGroup.getPositionOffset().x, roads[i].y + roadGroup.getPositionOffset().y));
+        }
+    },
+    // 加载下一组怪物
+    loadNextGroupMonster: function () {
+        if (GameManager.getGroup() > GameManager.getMaxGroup()) {
+            cc.log("GPMainLayer.loadNextGroupMonster(): 怪物添加完毕");
+            return;
+        }
+
+        // 弹出一组怪物数据，并保存在GameManager.currMonsterDataPool数组中。
+        GameManager.currMonsterDataPool = GameManager.popNextMonsterGroupData();
+        // 重置当前怪物节点
+        GameManager.currMonsterPool[GameManager.getGroup() - 1] = [];
+
+        // 重置当前组已经创建的怪物数量
+        this.currGroupCreatedMonsterCount = 0;
+        // 怪物总数设计
+        this.currGroupCreatedMonsterSum = GameManager.getCurrGroupMonsterSum();
+
+        let groupDelay = cc.delayTime(GameManager.getGroupInterval());
+        // 延迟时间
+        let enemyDelay = cc.delayTime(GameManager.getEnemyInterval());
+        let callback = cc.callFunc(this.createMonster.bind(this));
+        let createMonsterAction = cc.sequence(enemyDelay.clone(), callback).repeat(this.currGroupCreatedMonsterSum);
+        let finalAction = cc.sequence(groupDelay, createMonsterAction);
+        this.runAction(finalAction);
+    },
+    createMonster: function () {
+        let data = GameManager.currMonsterDataPool[0];
+        // 创建怪物数量 + 1
+        this.currGroupCreatedMonsterCount++;
+
+        let monsterData = {
+            road: this.roadPointArray,
+            speed: data.speed,
+            index: data.index
+        };
+
+        let namePrefix = data.name.substring(0, data.name.length - 1);
+        let fileNamePrefix = "Theme" + GameManager.getThemeID() + "/Monster" + namePrefix;
+        let fileName = "#" + fileNamePrefix + "1.png";
+        let node = new Monster(fileName, monsterData, fileNamePrefix);
+        this.addChild(node, this.ZOrderEnum.MONSTER);
+        GameManager.currMonsterPool[GameManager.getGroup() - 1].push(node);
+        node.run();
+
+        // 删除第一个数据
+        GameManager.currMonsterDataPool.splice(0, 1);
+    },
+    // 注册事件监听
+    registerEvent: function () {
+        // （事件监听）怪物吃到萝卜
+        let onMonsterEatCarrotListener = cc.EventListener.create({
+            event: cc.EventListener.CUSTOM,
+            target: this,
+            eventName: jf.EventName.GP_MONSTER_EAT_CARROT,
+            callback: this.onMonsterEatCarrot
+        });
+        cc.eventManager.addListener(onMonsterEatCarrotListener, this);
+
+        // （事件监听）萝卜血量更新
+        let onUpdateCarrotBloodListener = cc.EventListener.create({
+            event: cc.EventListener.CUSTOM,
+            target: this,
+            eventName: jf.EventName.GP_UPDATE_CARROT_BLOOD,
+            callback: this.onUpdateCarrotBlood
+        });
+
+        cc.eventManager.addListener(onUpdateCarrotBloodListener, this);
+    },
+    // 怪物吃到萝卜
+    onMonsterEatCarrot: function (event) {
+        let self = event.getCurrentTarget();
+        // 萝卜和血
+        GameManager.subtractCarrotBlood();
+        // 删除敌人
+        let monster = event.getUserData().target;
+        self.removeMonster(monster);
+        // 进入下一组
+        if (self.isNeedLoadNextGroup()) {
+            self.loadNextGroupMonster();
+        }
+    },
+    // 事件更新萝卜血量
+    onUpdateCarrotBlood: function (event) {
+        let self = event.getCurrentTarget();
+        let blood = event.getUserData().blood;
+        self.carrotHpText.setString(blood + "");
+    },
+    // 萝卜每次扣一滴血
+    subtractCarrotBlood: function () {
+        this.carrotBlood = this.carrotBlood <= 0 ? 0 : this.carrotBlood - 1;
+        // 抛出血量更新事件
+        let event = new cc.EventCustom(jf.EventName.GP_UPDATE_CARROT_BLOOD);
+        event.setUserData({
+            blood: this.carrotBlood
+        });
+        cc.eventManager.dispatchEvent(event);
+
+        // 抛出游戏结束事件
+        if (this.carrotBlood == 0) {
+            let gameOverEvnet = new cc.EventCustom(jf.EventName.GP_GAME_OVER);
+            gameOverEvnet.setUserData({
+                isWin: false
+            });
+            cc.eventManager.dispatchEvent(gameOverEvnet);
+        }
+    },
+    // 移除怪物
+    removeMonster: function (obj) {
+        let monster = null;
+        for (let i = 0; i < GameManager.currMonsterPool.length; i++) {
+            for (let j = 0; j < GameManager.currMonsterPool[i].length; i++) {
+                monster = GameManager.currMonsterPool[i][j];
+                if (monster == obj) {
+                    this.removeMonsterByIndex(i, j);
+                }
+            }
+        }
+    },
+    // 根据数组下标删除怪物
+    removeMonsterByIndex: function (i, j) {
+        this.removeChild(GameManager.currMonsterPool[i][j]);
+        GameManager.currBulletPool[i].splice(j, 1);
+    },
+    // 判断是否需要进入到下一组
+    isNeedLoadNextGroup: function () {
+        let isNeed = false;
+        if (GameManager.currMonsterPool[GameManager.group - 1].length == 0 && this.currGroupCreatedMonsterCount == this.currGroupCreatedMonsterSum) {
+            isNeed = true;
+        }
+        return isNeed;
     }
 });
---
+++ Carrot/src/Scene/GamePlay/Node/Monster/Monster.js
@@ -0,0 +1,52 @@
+let Monster = cc.Sprite.extend({
+    road: [], // 移动路径
+    data: {}, // 数据
+    speed: 0, // 速度
+    index: 0, // 索引
+    roadIndex: 0, // 当前移动路径的前缀
+    fileNamePrefix: "", // 帧前缀
+    ctor: function (fileName, data, fileNamePrefix) {
+        this._super(fileName);
+    },
+    // 加载属性配置
+    loadProperty: function (data, fileNamePrefix) {
+        cc.assert(data.speed, "Monster.loadProperty(): 速度不能为空!");
+        cc.assert(data.road, "Monster.loadProperty(): 移动路径不能为空！");
+        cc.assert(data.index >= 0, "Monster.loadProperty(): 索引不能为空！");
+        cc.assert(fileNamePrefix, "Monster.loadProperty(): 文件名前缀不能为空！");
+
+        this.data = data;
+        this.speed = data.speed;
+        this.road = data.road;
+        this.index = data.index;
+        this.fileNamePrefix = fileNamePrefix;
+    },
+    // 跑到下一个标记点上
+    runNextRoad: function () {
+        // 转方向
+        if (this.road[this.roadIndex].x <= this.load[this.roadIndex + 1].x) {
+            this.setFlippedX(false);
+        } else {
+            this.setFlippedX(true);
+        }
+
+        let distance = cc.pDistance(this.road[this.roadIndex], this.road[this.roadIndex + 1]);
+        let time = distance / this.speed;
+        let moveTo = cc.moveTo(time, this.road[this.roadIndex + 1]);
+        let callback = cc.callFunc(function () {
+            if (this.roadIndex < this.road.length - 1) {
+                this.runNextRoad();
+            } else {
+                // 吃到萝卜事件抛出
+                let event = new cc.EventCustom(jf.EventName.GP_MONSTER_EAT_CARROT);
+                event.setUserData({
+                    target: this
+                });
+                cc.eventManager.dispatchEvent(event);
+            }
+        }.bind(this));
+        let seq = cc.sequence(moveTo, callback);
+        this.runAction(seq);
+        this.roadIndex++;
+    }
+});
